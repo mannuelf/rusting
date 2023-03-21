d9e2b9b36d40d4edbe2de369bfbcb011abc15d82
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-03-21 08:26:09 +0100

    guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,13 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     // println! is macro
     println!("❓ Guessing the number");
     println!("Please input you guess");
+
+    let secrete_number = rand::thread_rng().gen_range(1..=100);
 
     // An associated function, function that's implimented on a type String:: in this case.
     let mut guess = String::new();
@@ -16,5 +20,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
