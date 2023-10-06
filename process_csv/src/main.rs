42e45d7775b08e13aa5b774a7d370fafa64aa64d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-10-06 16:04:10 +0200

    wip

--- process_csv/src/main.rs
+++ process_csv/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let penguin_data = "\
-   common name,length (cm)
-   Little penguin,33
-   Yellow-eyed penguin,65
-   Fiordland penguin,60
-   Invalid,data
-   ";
+    common name,length (cm)
+    Little penguin,33
+    Yellow-eyed penguin,65
+    Fiordland penguin,60
+    Invalid,data
+    ";
 
     let records = penguin_data.lines();
 
